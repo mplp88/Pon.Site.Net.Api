81db925a1800652a3bd35dc6bd62fce76e96fca3
Martín Ponce <martinp88@gmail.com>
2023-12-19 00:16:47 -0300

    Initial Commit

---
+++ Pon.Site.Net.Api/Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pon.Site.Net.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Api is working");
+        }
+    }
+}
